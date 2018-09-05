7050ff4f103ae665c57d8c35b4d153559a892cb6
Erik Dubois <erik.dubois@gmail.com>
2018-09-05 14:45:32 +0200

    config cinnamon adapted

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -24,8 +24,8 @@ sudo pacman -S arcolinux-arc-themes-git --noconfirm --needed
 sudo pacman -S arcolinux-bin-git --noconfirm --needed
 #sudo pacman -S arcolinux-bspwm-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
-#sudo pacman -S arcolinux-config-cinnamon-git --noconfirm --needed
-sudo pacman -S arcolinux-config-git --noconfirm --needed
+sudo pacman -S arcolinux-config-cinnamon-git --noconfirm --needed
+#sudo pacman -S arcolinux-config-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-gnome-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-mate-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-plasma-git --noconfirm --needed
