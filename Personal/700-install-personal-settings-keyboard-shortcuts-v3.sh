c62147e712b31b29454c14243618d9099f056d5d
Erik Dubois <erik.dubois@gmail.com>
2019-01-01 10:02:46 +0100

    more info

--- Personal/700-install-personal-settings-keyboard-shortcuts-v3.sh
+++ Personal/700-install-personal-settings-keyboard-shortcuts-v3.sh
@@ -6,8 +6,9 @@ set -e
 # Website	:	https://www.arcolinux.info
 # Website	:	https://www.arcolinux.com
 # Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
 # Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
