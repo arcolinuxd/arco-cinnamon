a49a8fd1bbeb9eef0ad0efb89f0a382fe47e9a81
Erik Dubois <erik.dubois@gmail.com>
2019-11-29 22:07:44 +0100

    compare

--- 200-software-arch-linux-repo-v2.sh
+++ 200-software-arch-linux-repo-v2.sh
@@ -96,6 +96,7 @@ sudo pacman -S --noconfirm --needed arc-gtk-theme
 sudo pacman -S --noconfirm --needed accountsservice
 #sudo pacman -S --noconfirm --needed archey3
 sudo pacman -S --noconfirm --needed baobab
+#sudo pacman -S --noconfirm --needed bleachbit
 sudo pacman -S --noconfirm --needed curl
 sudo pacman -S --noconfirm --needed dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode
